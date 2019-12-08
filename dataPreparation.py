dd1562835ebb5c56901d5566102d19a80c04c8ce
Aethor <oow.autre@gmail.com>
2019-12-08 22:50:42 +0800

    fft _prototype_

--- dataPreparation.py
+++ dataPreparation.py
@@ -1,6 +1,51 @@
+from typing import Generator, Tuple
 import cv2
 import numpy as np
 import os
+
+from numpy.fft import fft2, fftshift
+from keras_preprocessing.image.numpy_array_iterator import NumpyArrayIterator
+
+
+# FIXME: this function is a temporary _hack_
+# should be replaced when refactoring to use
+# generators everywhere
+def to_generator(
+    X: np.array, y: np.array, batch_size: int
+) -> Generator[Tuple[np.array, np.array], None, None]:
+    """
+    :param X:          (example_nb, image_width, image_height, colors_nb)
+    :param y:          (example_nb, classes_nb)
+    :param batch_size: 
+    :yield:
+    """
+    batch_nb = X.shape[0] // batch_size
+    for i in range(batch_nb):
+        cur_idx = i * batch_size
+        yield X[cur_idx : cur_idx + batch_size], y[cur_idx : cur_idx + batch_size]
+
+
+def fft_process(
+    batch_generator: Generator,
+) -> Generator[Tuple[np.array, np.array], None, None]:
+    """
+    :param batch_generator: 
+    :yield: tuple of np.array
+        new_X : (batch_size, image_width, image_height, 2)
+        y :     (batch_size, classes_nb)
+    """
+    for X, y in batch_generator:
+        batch_size = X.shape[0]
+        image_width = X.shape[1]
+        image_height = X.shape[2]
+        new_X = np.zeros((batch_size, image_width, image_height, 2))
+        # for each image in batch
+        for i in range(batch_size):
+            hann = np.mean(X[i], -1) * np.hanning(image_width)
+            tmp_fft = fftshift(fft2(hann))
+            new_X[i][:, :, 0] = np.log(np.abs(tmp_fft) ** 2)
+            new_X[i][:, :, 1] = np.arctan(tmp_fft.imag / tmp_fft.real)
+        yield new_X, y
 
 
 def preprocess(image, imageWidth, imageHeight):
@@ -10,23 +55,27 @@ def preprocess(image, imageWidth, imageHeight):
 def loadData(path, augment=False, preprocess=None):
     x = None
     y = np.empty(shape=(0,))
-    subDirs = [name for name in os.listdir(
-        path) if os.path.isdir(os.path.join(path, name))]
+    subDirs = [
+        name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))
+    ]
 
     for classNumber, subDir in enumerate(subDirs):
         tempX = []
         tempY = []
         classDirPath = os.path.join(path, subDir)
-        imageDir = [name for name in os.listdir(
-            classDirPath) if os.path.isfile(os.path.join(classDirPath, name))]
+        imageDir = [
+            name
+            for name in os.listdir(classDirPath)
+            if os.path.isfile(os.path.join(classDirPath, name))
+        ]
         for index, imageName in enumerate(imageDir):
             image = cv2.imread(os.path.join(classDirPath, imageName))
-            if (preprocess):
+            if preprocess:
                 image = preprocess[0](image, preprocess[1], preprocess[2])
 
             tempX.insert(index, image)
             tempY.insert(index, subDir)
-#             break;
+        #             break;
         if x is None:
             x = np.array(tempX)
         else:
